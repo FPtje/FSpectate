f740c2fb398e78d071924db8cd1ca237e2e63f2f
FPtje <fpeijnenburg@gmail.com>
2015-12-03 10:00:54 +0100

    Forgot AddCSLuaFile. Thanks oubliete

--- lua/autorun/fspectate.lua
+++ lua/autorun/fspectate.lua
@@ -2,6 +2,10 @@ include("sh_cami.lua")
 include("fspectate/sh_init.lua")
 
 if SERVER then
+    AddCSLuaFile("sh_cami.lua")
+    AddCSLuaFile("fspectate/cl_init.lua")
+    AddCSLuaFile("fspectate/sh_init.lua")
+
     include("fspectate/sv_init.lua")
 elseif CLIENT then
     include("fspectate/cl_init.lua")
