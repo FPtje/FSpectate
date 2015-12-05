ff96cc509b02f5900258ee9aa6ff0680d1200585
FPtje <fpeijnenburg@gmail.com>
2015-12-05 13:38:28 +0100

    Better squares around players

--- lua/fspectate/cl_init.lua
+++ lua/fspectate/cl_init.lua
@@ -327,8 +327,14 @@ local function drawHelp()
     if not isRoaming then return end
 
     if not IsValid(target) then return end
-    local mins, maxs = target:LocalToWorld(target:OBBMins()):ToScreen(), target:LocalToWorld(target:OBBMaxs()):ToScreen()
-    draw.RoundedBox(12, mins.x, mins.y, maxs.x - mins.x, maxs.y - mins.y, red)
+
+    local center = target:LocalToWorld(target:OBBCenter())
+    local eyeAng = EyeAngles()
+    local rightUp = eyeAng:Right() * 16 + eyeAng:Up() * 36
+    local topRight = (center + rightUp):ToScreen()
+    local bottomLeft = (center - rightUp):ToScreen()
+
+    draw.RoundedBox(12, bottomLeft.x, bottomLeft.y, math.max(20, topRight.x - bottomLeft.x), topRight.y - bottomLeft.y, red)
 end
 
 /*---------------------------------------------------------------------------
